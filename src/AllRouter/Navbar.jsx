1442f811dc9f1e5633b4d790404d429ac3694375
jitendra4545 <jitendra45.js45@gmail.com>
2024-04-24 17:36:29 +0530

    added

--- src/AllRouter/Navbar.jsx
+++ src/AllRouter/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link as Think } from '@chakra-ui/react'
-import {HashLink as Link} from 'react-router-hash-link'
+import { HashLink as Link } from 'react-router-hash-link'
 import {
   Box, Button, Heading, TabList, Tabs, Tab, useMediaQuery,
   useDisclosure,
@@ -14,7 +14,7 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-// Link
+  // Link
 
 
 } from '@chakra-ui/react'
@@ -42,13 +42,13 @@ function Navbar() {
     };
   }, []);
 
-  const handleOpen=()=>{
+  const handleOpen = () => {
     window.open(`https://drive.google.com/file/d/1XzPt59GbCmh5TxvAM5eEjxSHSyYT6Xdb/view?usp=sharing`)
   }
 
   return (
 
-    <Box  bg={isscroll ? "blue" : 'white'} zIndex='1000' position='sticky' top='0px' padding={{ base: '15px 30px', md: '15px 40px', lg: '10px 90px' }} boxShadow='rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, skyblue 0px 4px 6px -2px;' justifyContent='space-between' display='flex'>
+    <Box bg={isscroll ? "blue" : 'white'} zIndex='1000' position='sticky' top='0px' padding={{ base: '15px 30px', md: '15px 40px', lg: '10px 90px' }} boxShadow='rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, skyblue 0px 4px 6px -2px;' justifyContent='space-between' display='flex'>
 
       <Box >
         <Link smooth to="#home"><Heading fontStyle='italic' color={isscroll ? 'white' : "blue"}>Jitendra</Heading> </Link>
@@ -60,12 +60,12 @@ function Navbar() {
             <Tabs color={'blue'} variant='solid-rounded'>
               <TabList gap='3' >
 
-                <Link  smooth to="#home"><Tab color={isscroll ? 'white' : "#02054B"} >Home</Tab></Link>
+                <Link smooth to="#home"><Tab color={isscroll ? 'white' : "#02054B"} >Home</Tab></Link>
                 <Link smooth to='#about'><Tab color={isscroll ? 'white' : "#02054B"} >About</Tab></Link>
                 <Link smooth to='#skill'><Tab color={isscroll ? 'white' : "#02054B"} >Skills</Tab></Link>
                 <Link smooth to='#project'><Tab color={isscroll ? 'white' : "#02054B"} >Projects</Tab></Link>
                 <Link smooth to='#contact'><Tab color={isscroll ? 'white' : "#02054B"} >Contact</Tab></Link>
-                <Think onClick={handleOpen}  href={Resume}  download   ><Tab color={isscroll ? 'white' : "#02054B"}>Resume</Tab></Think>
+                <Think onClick={handleOpen} href={Resume} download   ><Tab color={isscroll ? 'white' : "#02054B"}>Resume</Tab></Think>
               </TabList>
             </Tabs>
           </Box>
@@ -88,11 +88,12 @@ function Navbar() {
                 <HamburgerIcon />
               </MenuButton>
               <MenuList fontSize='20px' fontWeight={'bold'} backgroundColor={'blue'}>
-                <Link  smooth to="#home"><MenuItem _focus={{ background: 'blue', color: "white" }}  >Home</MenuItem></Link>
+                <Link smooth to="#home"><MenuItem _focus={{ background: 'blue', color: "white" }}  >Home</MenuItem></Link>
                 <Link smooth to='#about'> <MenuItem _focus={{ background: 'blue', color: "white" }}>About</MenuItem></Link>
-                <Link smooth to='#project'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Projects</MenuItem></Link>
+
                 <Link smooth to='#skill'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Skills</MenuItem></Link>
-                <Link smooth to='#contact'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Contact Me</MenuItem></Link>
+                <Link smooth to='#project'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Projects</MenuItem></Link>
+                <Link smooth to='#contact'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Contact</MenuItem></Link>
                 <a href='https://drive.google.com/file/d/1iNwUQuC_PVgLBdaPtoDm5-mXL5VRd2Kf/view?usp=sharing'> <MenuItem _focus={{ background: 'blue', color: "white" }}>Resume</MenuItem></a>
               </MenuList>
             </Menu>
